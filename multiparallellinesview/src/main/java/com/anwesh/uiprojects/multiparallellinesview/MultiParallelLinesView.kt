a7da4da1d06cde52cf14f5e5b8838000c9872ec2
Anwesh43 <anweshthecool0@gmail.com>
2020-02-23 15:43:46 +0530

    adding MultiParallelLinesView class

--- multiparallellinesview/src/main/java/com/anwesh/uiprojects/multiparallellinesview/MultiParallelLinesView.kt
+++ multiparallellinesview/src/main/java/com/anwesh/uiprojects/multiparallellinesview/MultiParallelLinesView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawMPLNode(i : Int, scale : Float, paint : Paint) {
     drawMultiParallelLine(scale, size, paint)
     restore()
 }
+
+class MultiParallelLinesView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
