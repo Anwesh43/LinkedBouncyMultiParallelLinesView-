13b1178d5e8f839a3716918e895df5f53f26c01e
Anwesh43 <anweshthecool0@gmail.com>
2020-02-23 15:00:43 +0530

    adding constants and libraries

--- multiparallellinesview/src/main/java/com/anwesh/uiprojects/multiparallellinesview/MultiParallelLinesView.kt
+++ multiparallellinesview/src/main/java/com/anwesh/uiprojects/multiparallellinesview/MultiParallelLinesView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.multiparallellinesview
 /**
  * Created by anweshmishra on 23/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#4CAF50", "#1565C0", "#f44336", "#673AB7", "#EF6C00")
+val deg : Float = 90f
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
