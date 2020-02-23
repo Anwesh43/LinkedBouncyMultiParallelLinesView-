963f1a241e03a3409d2d463bbfca907d2d936dd7
Anwesh43 <anweshthecool0@gmail.com>
2020-02-23 15:10:38 +0530

    adding Canvas extension methods

--- multiparallellinesview/src/main/java/com/anwesh/uiprojects/multiparallellinesview/MultiParallelLinesView.kt
+++ multiparallellinesview/src/main/java/com/anwesh/uiprojects/multiparallellinesview/MultiParallelLinesView.kt
@@ -14,7 +14,7 @@ import android.graphics.Color
 
 val colors : Array<String> = arrayOf("#4CAF50", "#1565C0", "#f44336", "#673AB7", "#EF6C00")
 val deg : Float = 90f
-val nodes : Int = 5
+val lines : Int = 5
 val scGap : Float = 0.02f
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
@@ -25,3 +25,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawParallelLine(i : Int, scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, lines)
+    val gap : Float = (size / lines) * (i + 1)
+    for (j in 0..1) {
+        save()
+        translate(0f, gap * 0.5f * (1f - 2 * j) * sf)
+        rotate(90f * sf)
+        drawLine(0f, -gap / 2, 0f, gap / 2, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawMultiParallelLine(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawParallelLine(j, scale, size, paint)
+    }
+}
+
+fun Canvas.drawMPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h)
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = size / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, h / 2)
+    drawMultiParallelLine(scale, size, paint)
+    restore()
+}
